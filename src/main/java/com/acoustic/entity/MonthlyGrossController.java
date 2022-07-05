2bdd5d902c1c5739fa6fc2ed3d8bce6cf5e01db2
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-07-05 16:52:00 +0200

    modified GenerationType from Sequence to Indentity

--- src/main/java/com/acoustic/entity/MonthlyGrossController.java
+++ src/main/java/com/acoustic/entity/MonthlyGrossController.java
@@ -18,7 +18,7 @@ import java.math.BigDecimal;
 @Builder
 public class MonthlyGrossController {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private BigDecimal monthlyGrossAmount;
