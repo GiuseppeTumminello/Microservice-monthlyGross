6245233fded4da6c6174125b4d5fdb42304af9a6
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-07-15 10:37:43 +0200

    changed properties adding NATIVE

--- src/main/java/com/acoustic/controller/MonthlyGrossController.java
+++ src/main/java/com/acoustic/controller/MonthlyGrossController.java
@@ -21,13 +21,14 @@ public class MonthlyGrossController {
 
     public static final String DESCRIPTION = "description";
     public static final String VALUE = "value";
+    public static final int MINIMUM_GROSS = 2000;
 
     private final MonthlyGrossRepository monthlyGrossRepository;
     private final SalaryCalculatorService salaryCalculatorService;
 
 
     @PostMapping("/calculation/{grossMonthlySalary}")
-    public Map<String, String> calculateMonthlyGross(@PathVariable @Min(2000)BigDecimal grossMonthlySalary){
+    public Map<String, String> calculateMonthlyGross(@PathVariable @Min(MINIMUM_GROSS)BigDecimal grossMonthlySalary){
         var monthlyGross = this.salaryCalculatorService.apply(grossMonthlySalary);
         this.monthlyGrossRepository.save(com.acoustic.entity.MonthlyGrossController.builder().monthlyGrossAmount(monthlyGross).build());
         return Map.of(DESCRIPTION,this.salaryCalculatorService.getDescription(), VALUE, String.valueOf(monthlyGross));
